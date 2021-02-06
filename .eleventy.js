0e328c26b87d79eeb385219996ec78b1600be931
Amey Bhavsar <iamamey24@gmail.com>
2021-02-06 11:39:42 +0530

    added js and scss configs and pre-processors

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,24 @@
+const dev = global.dev  = (process.env.ELEVENTY_ENV === 'dev');
+
 module.exports = config =>  {
-	config.addPassthroughCopy('src/js');
-	config.addPassthroughCopy('src/scss');
+
+	/* --- TRANSFORMS -- */
+
+	// inline assets
+	config.addTransform('inline', require('./lib/transforms/inline'));
+
+	// minify HTML
+	config.addTransform('htmlminify', require('./lib/transforms/htmlminify'));
+
+	// CSS processing
+	config.addTransform('postcss', require('./lib/transforms/postcss'));
+  	
+	/* --- WATCH FOLDERS --- */
+
+	config.addWatchTarget('./src/scss/');
+	config.addWatchTarget('./src/js/');
+
+	
 	// 11ty defaults
 	return {
 		dir: {
